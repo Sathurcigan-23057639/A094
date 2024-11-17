eb2b6ff3208b800ca80332d1e948958c819cfd24
KavinduPorambage <kp24aax@herts.ac.uk>
2024-11-17 23:30:50 +0530

    Histogram (temp.R)

--- temp.R
+++ temp.R
@@ -16,7 +16,7 @@ hist(FIFA_2018_Statistics$`Goal Scored`, main = "Goal Scored Frequency")
 cor.test(FIFA_2018_Statistics$`Ball Possession %`, FIFA_2018_Statistics$`Goal Scored`,
          method="spearman")
 
-# Histogram 
+# Histogram with normal curve
 h <- hist(df$`Goal Scored`, 
           breaks = 6, 
           main = "Goal Scored Histogram", 
