c62409103bf54e78ed3a1043a658c30cd59e1737
Sathurcigan Kamalendran <sk24abn@herts.ac.uk>
2024-11-17 15:57:57 +0000

    Scatterplot creation

--- temp.R
+++ temp.R
@@ -1,1 +1,16 @@
+#show data set
 head(FIFA_2018_Statistics,2)
+
+#Scatterplot 
+plot(FIFA_2018_Statistics$`Ball Possession %`, FIFA_2018_Statistics$`Goal Scored`,
+     xlab = "Ball Possession %", ylab =
+       "Goal Scored", main
+     = "Scatterplot of Ball Possession % vs Goal Scored") #
+
+abline(lm(FIFA_2018_Statistics$`Goal Scored` ~
+            FIFA_2018_Statistics$`Ball Possession %`), col = "red")
+
+#Histogram
+hist(FIFA_2018_Statistics$`Goal Scored`, main = "Goal Scored Frequency")
+cor.test(FIFA_2018_Statistics$`Ball Possession %`, FIFA_2018_Statistics$`Goal Scored`,
+         method="spearman")
